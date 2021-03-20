09ebdae1e3eb81c512cda6233327966347b71146
ninesky00 <jiangzho@gmail.com>
2021-03-19 21:21:27 -0500

    finished commit

---
+++ db/seeds/dev/papers.js
@@ -0,0 +1,25 @@
+
+exports.seed = function(knex) {
+  return knex('footnotes').del() // delete all footnotes first
+  .then(() => knex('papers').del()) // delete all papers
+
+  // Now that we have a clean slate, we can re-insert our paper data
+  .then(() => {
+    return Promise.all([
+
+      // Insert a single paper, return the paper ID, insert 2 footnotes
+      knex('papers').insert({
+        title: 'Fooo', author: 'Bob', publisher: 'Minnesota'
+      }, 'id')
+      .then(paper => {
+        return knex('footnotes').insert([
+          { note: 'Lorem', paper_id: paper[0] },
+          { note: 'Dolor', paper_id: paper[0] }
+        ])
+      })
+      .then(() => console.log('Seeding complete!'))
+      .catch(error => console.log(`Error seeding data: ${error}`))
+    ]) // end return Promise.all
+  })
+  .catch(error => console.log(`Error seeding data: ${error}`));
+};
